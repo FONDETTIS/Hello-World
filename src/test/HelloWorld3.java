e50f19a5662b18a07c0f1a7f4a33c5812614717b
COUDERT <florent.coudert37@gmail.com>
2013-11-23 15:42:18 +0100

    Update HelloWorld3.java

--- src/test/HelloWorld3.java
+++ src/test/HelloWorld3.java
@@ -34,7 +34,7 @@ package test;
 public class HelloWorld3 {
 	
 	 public static void main(String[] args) {
-	        System.out.println("Hello, World 3");
+	        System.out.println("Hello, World  develop");
 	    }
 
 }
